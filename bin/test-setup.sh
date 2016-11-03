aee527bb9707c5d4490fef28ba19ce1df6aa69c2
Nolan Lawson <nolan.lawson@gmail.com>
2016-11-03 09:36:22 -0700

    try again

--- bin/test-setup.sh
+++ bin/test-setup.sh
@@ -3,11 +3,10 @@
 # install pouchdb from git master rather than npm,
 # so we can run its own tests
 rm -fr node_modules/pouchdb
-git clone --single-branch --branch master \
+git clone --single-branch --branch master --depth 1 \
   https://github.com/pouchdb/pouchdb.git node_modules/pouchdb
 
 cd node_modules/pouchdb/
-git reset --hard c3b79b9
 npm install
 cd node_modules/pouchdb-server/node_modules
 rm -fr express-pouchdb
